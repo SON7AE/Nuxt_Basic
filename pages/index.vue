e88304fff934a1920c7c6443355056477386be49
Marco <bobsot0225@gmail.com>
2022-07-22 18:29:31 +0900

    2022. 07. 22. Nuxt에서 Vuex 사용하기

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
+  <!-- <div>
     <h1 class="text-lg font-bold">Main</h1>
     <p>이 문장은 Nuxt.js Version2를 활용한 Main Page 코드입니다.</p>
+  </div> -->
+
+  <div>
+    <pre>
+      {{ $store.state.user }}
+    </pre>
+
+    <pre>
+      {{ user }}
+    </pre>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "IndexPage",
+
+  computed: {
+    ...mapState({
+      user: (state) => state.user,
+    }),
+  },
 };
 </script>
