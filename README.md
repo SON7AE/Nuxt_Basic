3e392d9dcfd08d85f13cd210cd4a511f10e886bd
Marco <bobsot0225@gmail.com>
2022-07-22 17:32:01 +0900

    2022. 07. 22. Nuxt AsyncData + $axios 사용

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 ## 2. Dynamic Routing
 
 - 동적라우팅(Dynamic Routing)은 동일한 페이지를 여러 개 만드는 경우(예를 들어 사용자의 이름만 다른 중복된 페이지를 여러 개 만드는 경우) 사용자 마다 모든 페이지를 하나하나 만드는 것은 사실상 불가능하기 때문에 이런 경우 우리는 URL을 동적으로 생성해주어야 한다. 그렇기때문에 `동적 URL을 만들기 위해서는 이름 앞에 언더바 키워드(_)를 사용`한다.
+
+## 3. AsyncData
+
+- asyncData 메서드의 정의
+  asyncData 메서드는 Page Components에서만 사용이 가능하며 비동기 처리에 의해 얻어진 데이터를 SSR(Server Side Rendering)하기 위해 사용되는 훅이다.
+  첫 번째 Parameter로 context가 온다. asyncData는 컴포넌트에 데이터를 셋팅하는 것을 목적으로 사용된다.
+  따라서 asyncData에 의해 반환된 값은 컴포넌트의 템플릿에서 접근이 가능하다.
+
+- `npm install @nuxtjs/axios`
+- nuxt.config.js 파일 안에 `modules: ['@nuxtjs/axios']`를 추가한다.
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -4,13 +4,48 @@
       <h1 class="text-lg font-bold">Products</h1>
       <NuxtLink to="/products/product-a">Product A</NuxtLink>
       <NuxtLink to="/products/product-b">Product B</NuxtLink>
+
+      <br />
+      <br />
+
+      <div
+        v-for="product in products"
+        :key="product.id"
+        class="border-b border-gray-400 py-3"
+      >
+        {{ product.title }}
+      </div>
+
       <p>이 문장은 Nuxt.js Version2를 활용한 Products Page입니다.</p>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "",
+
+  data() {
+    return {};
+  },
+
+  // asyncData 메서드의 정의
+  // asyncData 메서드는 Page Components에서만 사용이 가능하며 비동기 처리에 의해 얻어진 데이터를 SSR(Server Side Rendering)하기 위해 사용되는 훅이다.
+  // 첫 번째 Parameter로 context가 온다.
+  // asyncData는 컴포넌트에 데이터를 셋팅하는 것을 목적으로 사용된다.
+  // 따라서 asyncData에 의해 반환된 값은 컴포넌트의 템플릿에서 접근이 가능하다.
+  async asyncData({ $axios }) {
+    const products = await $axios.$get(
+      "https://jsonplaceholder.typicode.com/posts?_limit=3"
+    );
+
+    return {
+      products,
+    };
+  },
+
+  methods: {},
+};
 </script>
 
 <style></style>
